d458455d640d878e8148604acc689f4c7b321824
Jordan337 <JordanJoyce486@gmail.com>
2025-05-28 16:29:20 +0100

    unrevert

---
+++ GitHubSearch/App_Start/UnityConfig.cs
@@ -0,0 +1,26 @@
+﻿using GitHubSearch.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Unity;
+
+public static class UnityConfig
+{
+    private static Lazy<IUnityContainer> container = new Lazy<IUnityContainer>(() =>
+    {
+        var container = new UnityContainer();
+        RegisterTypes(container);
+        return container;
+    });
+
+    public static IUnityContainer Container => container.Value;
+
+    public static void RegisterTypes(IUnityContainer container)
+    {
+        // Register your types here
+        // e.g. container.RegisterType<IProductRepository, ProductRepository>();
+
+        container.RegisterType<IGitHubService, GitHubService>();
+    }
+}
