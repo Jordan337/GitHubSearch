67f56de0d1092ce0d82dc503bc804916998966ca
Jordan337 <JordanJoyce486@gmail.com>
2025-05-28 16:07:01 +0100

    Revert "BaseProject not Optimised as of yet"
    This reverts commit 58663d24bac0b826260a71f5d0e60250952f509d.

--- GitHubSearch/Global.asax.cs
+++ GitHubSearch/Global.asax.cs
@@ -16,7 +16,6 @@ namespace GitHubSearch
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
         }
     }
 }
